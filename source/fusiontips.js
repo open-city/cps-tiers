87b0f924dff1d22c005283368992a40919c5ea06
Derek Eder <derek.eder@gmail.com>
2013-02-22 13:44:10 -0600

    style fix for tier legend

--- source/fusiontips.js
+++ source/fusiontips.js
@@ -228,7 +228,7 @@
     function queryFusionJson(latlng, queryText) {
       var script = document.createElement('script');
       // Note that a simplified geometry and the NAME column are being requested
-      //http://www.google.com/fusiontables/api/query?sql=
+      //http://www.fusiontables.googleusercontent.com/fusiontables/api/query?sql=
       var sid = 'query_' + scriptid++;
       script.setAttribute('src', 'https://www.googleapis.com/fusiontables/v1/query?sql=' + queryText + '&callback=fusiontips.' + sid + '&key=' + opts.googleApiKey + "&typed=false");
       //script.setAttribute('src', 'http://fusiontables.googleusercontent.com/fusiontables/api/query?sql=' + queryText + '&jsonCallback=fusiontips.' + sid);
